5b2107941af3ca1af17e18a1351c47191a1e6f11
Kazuma Takata <neilbuzztakata@gmail.com>
2018-08-01 14:44:09 +0900

    complete db module without test

--- db/db.php
+++ db/db.php
@@ -22,18 +22,57 @@ class Database
 
     }
 
-    public function insertUser()
+    public function addFriend($myid, $friendid)
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->users;
+
+        $collection->updateOne(
+            ["_id" => new MongoDB\BSON\ObjectID($myid)],
+            ["$push" => ["friendIds" => new MongoDB\BSON\ObjectID($friendid)]]
+        );
+    }
+
+    public function insertUser($name, $pass)
     {
         $db = $this->client->swiftline;
         $collection = $db->users;
 
 
         $document = array(
-            "name" => "sample_name",
-            "password" => "sample_password"
+            "name" => $name,
+            "password" => $pass,
+            "photourl" => "http://localhost:8181/img/defaultprofile.png",
+            "backgroundurl" => "http://localhost:8181/img/rocco-caruso-722282-unsplash.jpg",
+            "friendIds" => [],
+            "talks" => [],
+
         );
         $collection->insertOne($document);
     }
+
+    public function insertFeed($feedcontent, $userid, $photourl)
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->feeds;
+
+
+        $document = ["feedcontent", $feedcontent, "userid", $userid, "photourl", $photourl];
+
+        $insertresult = $collection->insertOne($document);
+    }
+
+    public function insertTalk($friendid, $content, $myid)
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->feeds;
+
+        $insertResult = $collection->updateOne(
+            ["_id" => new MongoDB\BSON\ObjectID($myid)],
+            ["$push" => ["talks" => ["id" => new MongoDB\BSON\ObjectID($friendid), "content" => $content]]]
+        );
+    }
+
 
     public function findUser()
     {
@@ -46,6 +85,54 @@ class Database
         foreach ($users as $user) {
             var_dump($user);
         }
+    }
+
+    public function findOneByName($name)
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->users;
+
+        $Query = ["name", $name];
+        $user = $collection->findOne($Query);
+        return $user;
+    }
+
+    public function findOneById($id)
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->users;
+
+        $Query = ["_id", new MongoDB\BSON\ObjectID($id)];
+        $user = $collection->findOne($Query);
+        return $user;
+    }
+
+    public function find()
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->users;
+
+        $user = $collection->find();
+        return $user;
+    }
+
+    public function getFeeds()
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->feeds;
+
+        $findresult = $collection->find();
+
+        return $findresult;
+    }
+
+    public function RemoveAll()
+    {
+        $db = $this->client->swiftline;
+        $collection = $db->users;
+
+        $deleteResult = $collection->deleteMany();
+
     }
 
 }
--- test/loginTest.php
+++ test/loginTest.php
@@ -10,12 +10,12 @@ final class loginTest extends TestCase
 
     public function testAddisOk() : void
     {
-        $client = new \GuzzleHttp\Client();
-        $res = $client->post('http://localhost:8000/login', [
-            GuzzleHttp\RequestOptions::JSON => ['foo' => 'bar']
-        ]);
+        // $client = new \GuzzleHttp\Client();
+        // $res = $client->post('http://localhost:8000/login', [
+        //     GuzzleHttp\RequestOptions::JSON => ['foo' => 'bar']
+        // ]);
 
-        echo $res->getBody();
+        // echo $res->getBody();
         $this->assertEquals(add(3, 6), 9);
 
     }
